ccef1f09969054ca8702a20d4d61fae2c6c0d8cf
BagrijOstap <bagrijostap@gmail.com>
2019-06-25 10:57:32 +0300

    add state fo app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  state = {
+    todoList: [],
+  };
+
   render() {
-    return <h3>vecfsvvr</h3>
-  }
-}
+    return (<h3>vecfsvvr</h3>);
+  };
+
+
+};
 
 export default App;
