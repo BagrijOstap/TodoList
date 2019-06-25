cdb91cb2ac9cda08a92814681cf1c51caa73bb3e
BagrijOstap <bagrijostap@gmail.com>
2019-06-25 11:33:50 +0300

    updatet babel config and add todoList data

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import React, { Component } from 'react';
 class App extends Component {
 
   state = {
-    todoList: [],
+    todoList: [
+      {title: 'todo1', description: 'buyCar', isDone: false,},
+      {title: 'todo2', description: 'checkCar', isDone: false,},
+      {title: 'todo3', description: 'carWash', isDone: false,},
+      {title: 'todo4', description: 'repairCar', isDone: false,},
+    ],
   };
 
   render() {
     return (<h3>vecfsvvr</h3>);
   };
-
-
-};
+}
 
 export default App;
