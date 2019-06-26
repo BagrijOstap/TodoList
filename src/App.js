6f0d83d54973192756c1152bb238202c74037a0b
BagrijOstap <bagrijostap@gmail.com>
2019-06-26 10:55:19 +0300

    create function delete to remove todoList item

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import TodoItem from './components/TodoItem'
 
 class App extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.removeListItemByIndex = this.removeListItemByIndex.bind(this);
+  };
+
   state = {
     todoList: [
       {title: 'todo1', description: 'buyCar', isDone: false,},
@@ -12,6 +18,13 @@ class App extends Component {
       {title: 'todo4', description: 'repairCar', isDone: false,},
     ],
   };
+
+  removeListItemByIndex (removeIndex) {
+    const todoListLink = this.state.todoList;
+    const todoList = [...todoListLink];
+    todoList.splice(removeIndex, 1);
+    this.setState({ todoList: todoList })
+  }
 
   render() {
     const todoList = this.state.todoList;
